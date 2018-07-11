8802ef70c4f8dbb0f933dbe9c11958b2f27c5e31
Hsinchang.Ho <mkjeff@outlook.com>
2018-07-11 21:47:01 +0800

    target to netcoreapp

--- secs4net/Core/CIM.EAP/Data/SlotInfo.cs
+++
@@ -1,9 +0,0 @@
-using System.Diagnostics;
-namespace Cim.Eap.Data {
-    [DebuggerDisplay("{SlotNo} {Measure? \"[m]\" : \"\" ,nq}", Name = "{WaferID,nq}")]
-    public struct SlotInfo {
-        public byte SlotNo;
-        public string WaferID;
-        public bool Measure;
-    }
-}
---
+++ secs4net/Secs4Net.Json/SecsMessageConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Secs4Net.Json
+{
+	public sealed class SecsMessageConverter : JsonConverter<SecsMessage>
+	{
+		public override SecsMessage ReadJson(JsonReader reader, Type objectType, SecsMessage existingValue, bool hasExistingValue, JsonSerializer serializer)
+		{
+			return reader.ToSecsMessage();
+		}
+
+		public override void WriteJson(JsonWriter writer, SecsMessage value, JsonSerializer serializer)
+		{
+			value.WriteTo(writer);
+		}
+	}
+}
