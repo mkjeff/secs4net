e3a295498c8f041536024ec18ec66547578704db
TiltonJH <TiltonJH@users.noreply.github.com>
2019-08-27 18:13:33 +0200

    added AssemblyInfo.cs for assembly attributes

---
+++ secs4net/Core/Secs4Net/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9a19103f-16f7-4668-be54-9a1e7a4f7556")]
+
+// Make internals accessible to test methods.
+[assembly: InternalsVisibleTo("Secs4Net.Tests")]
